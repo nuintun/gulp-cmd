bcb8a9616db0e4e1a44680b2df30a29b3f2c2485
Nuintun <nuintun@qq.com>
2015-05-09 00:37:03 +0800

    add source file

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -13,6 +13,15 @@ var include = require('../lib/include');
 
 function listen(){
   return through.obj(function (file, encoding, done){
+    if (file.isNull()) {
+      // return empty file
+      done(null, file);
+    }
+
+    if (file.isStream()) {
+      return callback(util.throwError('streaming not supported.'));
+    }
+
     console.log(colors.infoBold(file.path));
     console.log('\n');
     console.log(colors.verboseBold(JSON.stringify(file.package, null, 2)));
@@ -28,7 +37,7 @@ var alias = { 'class': 'base/class/1.2.0/class' };
 gulp.task('default', function (){
   gulp.src('assets/js/**/*.*', { base: 'assets/js' })
     .pipe(transport(util.extendOption({ wwwroot: './', alias: alias, css2js: true })))
-    .pipe(include(util.extendOption({ wwwroot: './', alias: alias, css2js: true })))
+    .pipe(listen())
     //.pipe(gulp.dest('dist/js'))
     .on('end', function (){
       console.log('  ---------------all transport end---------------');
