ae6a2d3c87d67ccd4dd519fd8f3af4a1df3b0297
Nuintun <nuintun@qq.com>
2015-05-08 17:23:49 +0800

    add source files

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -59,12 +59,12 @@ function listen(){
 
 gulp.task('default', function (){
   gulp.src('assets/js/**/*.*', { base: 'assets/js' })
-    .pipe(transport(extendOption({ css2js: true })))
-    .pipe(listen()).pipe(gulp.dest('dist/js'));
+    .pipe(transport(extendOption({ css2js: true })));
+  //.pipe(listen()); //.pipe(gulp.dest('dist/js'));
 
-  //gulp.src('assets/css/**/*.*', { base: 'assets/css' })
-  //  .pipe(transport(extendOption()))
-  //  .pipe(listen()).pipe(gulp.dest('dist/css'));
+  gulp.src('assets/css/**/*.*', { base: 'assets/css' })
+    .pipe(transport(extendOption()));
+  //.pipe(listen()); //.pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('watch', function (){
