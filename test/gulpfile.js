44db4daf1f8ece2e0bd712225d0c963164b33e0f
Nuintun <nuintun@qq.com>
2015-05-09 23:45:15 +0800

    add source file

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -35,26 +35,29 @@ function listen(){
 var alias = { 'class': 'base/class/1.2.0/class' };
 
 gulp.task('default', function (){
-  gulp.src('assets/js/**/base.js', { base: 'assets/js' })
-    .pipe(include(util.extendOption({ wwwroot: './', alias: alias, css2js: true })))
+  gulp.src('assets/js/**/*.*', { base: 'assets/js' })
+    .pipe(include({ alias: alias }))
     .pipe(listen())
     //.pipe(gulp.dest('dist/js'))
     .on('end', function (){
       console.log('  ---------------all transport end---------------');
     });
 
-  //gulp.src('assets/css/**/*.*', { base: 'assets/css' })
-  //  .pipe(transport(util.extendOption({ wwwroot: './' })))
-  //  //.pipe(gulp.dest('dist/css'))
-  //  .on('end', function (){
-  //    console.log('  ---------------all transport end---------------');
-  //  });
+  gulp.src('assets/css/**/*.*', { base: 'assets/css' })
+    .pipe(include())
+    .pipe(listen())
+    //.pipe(gulp.dest('dist/css'))
+    .on('end', function (){
+      console.log('  ---------------all transport end---------------');
+    });
 });
 
 gulp.task('watch', function (){
   gulp.watch('assets/js/**/*.*', function (e){
     if (e.type !== 'deleted') {
       return gulp.src(e.path, { base: 'assets/js' })
+        .pipe(include({ alias: alias }))
+        .pipe(listen())
         //.pipe(gulp.dest('dist/js'))
         .on('end', function (){
           console.log('  ---------------all transport end---------------');
@@ -65,6 +68,8 @@ gulp.task('watch', function (){
   gulp.watch('assets/css/**/*.*', function (e){
     if (e.type !== 'deleted') {
       return gulp.src(e.path, { base: 'assets/css' })
+        .pipe(include())
+        .pipe(listen())
         //.pipe(gulp.dest('dist/css'))
         .on('end', function (){
           console.log('  ---------------all transport end---------------');
