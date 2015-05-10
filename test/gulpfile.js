404bd001b77ed1999637e82d914a00c235f6f2df
Nuintun <nuintun@qq.com>
2015-05-10 15:58:04 +0800

    add source file

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -10,17 +10,17 @@ var colors = util.colors;
 var transport = require('../index');
 
 var alias = { 'base': 'base/base/1.2.0/base' };
+var options = {
+  alias: alias,
+  include: function (id){
+    return id.indexOf('view') ? 'all' : 'relative';
+  }
+};
 
 gulp.task('default', function (){
-  gulp.src('assets/js/!(view)/**/*.!(css|json|tpl|html)', { base: 'assets/js' })
-    .pipe(transport({ alias: alias, include: 'relative' }))
-    .pipe(gulp.dest('dist/js'))
-    .on('end', function (){
-      console.log(colors.verboseBold('  gulp-cmd ') + colors.infoBold('build complete ...'));
-    });
 
-  gulp.src('assets/js/view/**/*.!(css|json|tpl|html)', { base: 'assets/js' })
-    .pipe(transport({ alias: alias, include: 'all' }))
+  gulp.src('assets/js/!(view)/**/*.!(css|json|tpl|html)', { base: 'assets/js' })
+    .pipe(transport(options))
     .pipe(gulp.dest('dist/js'))
     .on('end', function (){
       console.log(colors.verboseBold('  gulp-cmd ') + colors.infoBold('build complete ...'));
