cf3adc0693a8ba272bb0dd285ddea10ca5e291da
Nuintun <nuintun@qq.com>
2015-05-10 11:44:04 +0800

    add source file

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -32,7 +32,7 @@ function listen(){
   });
 }
 
-var alias = { 'class': 'base/class/1.2.0/class' };
+var alias = { 'class': 'base/base/1.2.0/class' };
 
 gulp.task('default', function (){
   gulp.src('assets/js/**/*.*', { base: 'assets/js' })
